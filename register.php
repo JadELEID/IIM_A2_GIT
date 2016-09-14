3059c7464423c56ef54d2911f6306b2388f59fed
Conrad <jarl.conrad@gmail.com>
2016-09-14 17:25:13 +0200

    Finalisation de l'inscription

--- register.php
+++ register.php
@@ -2,9 +2,39 @@
 require('config/config.php');
 require('model/functions.fn.php');
 
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
 /*===============================
 	Register
 ===============================*/
+
+if(isset($email) && isset($password) && isset($username)){
+	if(!empty($email) && !empty($password) && !empty($username)){
+
+		// TODO
+
+		// Force user connection to access dashboard
+
+		if(isUsernameAvailable($db, $username) && isEmailAvailable($db, $email)){
+			userRegistration($db, $username, $email, $password);
+			userConnection($db, $email, $password);
+			header('Location: dashboard.php');
+
+		} else if(isUsernameAvailable($db, $username)==false) {
+			$error = 'Username déjà pris';
+
+
+		} else if(isEmailAvailable($db, $email)==false) {
+			$error = 'Email déjà pris';
+
+		}
+
+	}else{
+		$error = 'Champs requis !';
+	}
+}
+
 
 include 'view/_header.php';
 include 'view/register.php';
